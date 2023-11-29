2f8ad34a7377986ed01d7fe28678f14b041de2af
Onile-ere Victor <116771477+Majorplaydev@users.noreply.github.com>
2023-11-29 16:26:20 +0100

    update error for list.h in check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,29 @@
 #include "lists.h"
 
 /**
- *  * check_cycle - this checks if a linked list
- *   * contains a cycle
- *    * @list: The linked list to check
- *     *
- *      * By : MajorPladev
- *       * Return: 1 if the list has a cycle, 0 if it
- *        * doesn't
- *         */
+ * check_cycle - this checks if a linked list
+ * contains a cycle
+ * @list: The linked list to check
+ *
+ * By : Majorplaydev
+ * Return: 1 if the list has a cycle, 0 if it
+ * doesn't
+ */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+listint_t *slow = list;
+listint_t *fast = list;
 
-	if (!list)
-		return (0);
+if (!list)
+return (0);
 
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
-	}
-	return (0);
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
 }
